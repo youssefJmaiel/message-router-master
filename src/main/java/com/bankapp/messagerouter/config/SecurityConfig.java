0c48694f5266dda46a3fb8350879c43626653773
youssef <youssef.jemaiel@nearteam.fr>
2026-03-03 00:15:33 +0100

    security configuration edits

--- src/main/java/com/bankapp/messagerouter/config/SecurityConfig.java
+++ src/main/java/com/bankapp/messagerouter/config/SecurityConfig.java
@@ -6,13 +6,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
